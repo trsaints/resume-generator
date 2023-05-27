7556d623ed5404b274790dea878eb7ab07d25d4d
trsaints <thiago.dev2k3@gmail.com>
2023-05-27 14:11:33 -0300

    Basic UI interaction

--- app/src/controller/controller.js
+++ app/src/controller/controller.js
@@ -2,6 +2,21 @@ export function initialize(deps) {
   init(deps);
 }
 
-function init(deps) {
+function init({ callbacks, components }) {
+  const form = callbacks.getElement("form");
 
+  const actions = {
+    generate: () => callbacks.generateResume({ components, form }),
+    setExperience: () => callbacks.showModal("experiences"),
+    setDegree: () => callbacks.showModal("degrees"),
+    cancel: ({ dataset }) => callbacks.closeModal(dataset.cancel),
+  };
+
+  form.addEventListener("submit", (e) => e.preventDefault());
+
+  document.addEventListener("click", ({ target }) => {
+    const { action } = target.dataset;
+
+    if (actions[action]) actions[action](target);
+  });
 }
--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,31 @@
 import { initialize } from "./controller/controller.js";
-
+import Resume from "./models/Resume.js";
+import { generateResume } from "./services/resume_service.js";
+import {
+  clearContent,
+  closeModal,
+  getElement,
+  getElements,
+  hideElement,
+  showElement,
+  showPopup,
+  showModal
+} from "./views/dom_view.js";
 const dependencies = {
-
-}
+  callbacks: {
+    closeModal,
+    getElement,
+    getElements,
+    clearContent,
+    hideElement,
+    showElement,
+    showPopup,
+    generateResume,
+    showModal
+  },
+  components: {
+    Resume,
+  },
+};
 
 initialize(dependencies);
