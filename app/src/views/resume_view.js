e20fdb7aaca3671e32fd2102cb91b0157094d088
trsaints <thiago.dev2k3@gmail.com>
2023-06-01 22:16:09 -0300

    Removal of unused code

--- app/src/views/resume_view.js
+++ app/src/views/resume_view.js
@@ -1,17 +1,3 @@
-export function renderExperience({ callbacks, components, exp }) {
-  const panel = callbacks.getElement("experiences-list");
-  const experienceCard = new components.ExperienceCard(exp);
-
-  panel.appendChild(experienceCard);
-}
-
-export function renderDegree({ callbacks, components, deg }) {
-  const panel = callbacks.getElement("degrees-list");
-  const degreeCard = new components.DegreeCard(deg);
-
-  panel.appendChild(degreeCard);
-}
-
 export function displayConfirmation(callbacks, target) {
   const card =
     target.closest("[data-element='experience']") ||
