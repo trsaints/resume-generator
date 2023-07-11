41c10b77684a64cc8bcd890345b891182fbb39b8
trsaints <thiago.dev2k3@gmail.com>
2023-07-11 17:53:54 -0300

    Data validation handling on submit

--- app/src/controllers/form_controller.js
+++ app/src/controllers/form_controller.js
@@ -2,72 +2,6 @@ export default function initialize(deps) {
   window.addEventListener("load", () => init(deps));
 }
 
-function init({ callbacks, components, models }) {
-  const form = callbacks.getElement("form");
-
-  setInitialState(callbacks, form);
-  setClickActions(callbacks, components, models);
-  setInputMonitoring(callbacks, form);
-  setBaseDataSync(callbacks, form);
-}
-
-function setClickActions(callbacks, components) {
-  const actions = {
-    openMenu: ({ dataset }) => callbacks.openMenu(dataset.dialog),
-    closeMenu: ({ dataset }) => callbacks.closeMenu(dataset.dialog),
-    addItem: ({ dataset }) => addItem(dataset, callbacks, components),
-    displayConfirmation: (target) =>
-      callbacks.displayConfirmation(callbacks, target),
-    removeItem: (target) => removeItem(target, callbacks, components),
-  };
-
-  document.addEventListener("click", ({ target }) => {
-    const { action } = target.dataset;
-
-    if (actions[action]) actions[action](target);
-  });
-}
-
-function setInputMonitoring(callbacks, form) {
-  form.addEventListener("input", ({ target }) => {
-    callbacks.updateCharacterCount(callbacks, target);
-    validateInput(callbacks, target);
-  });
-}
-
-function setInitialState(callbacks, form) {
-  callbacks.clearActiveResume(form);
-  callbacks.resetIds();
-  callbacks.clearForm(callbacks);
-}
-
-function setBaseDataSync(callbacks, form) {
-  const syncData = ({ target }) => {
-    const validChanges = ["name", "job", "address", "email", "website", "desc"],
-      shouldNotSync = !validChanges.includes(target.id);
-
-    if (shouldNotSync) return;
-
-    const baseData = callbacks.getBaseData(form);
-    localStorage.setItem("resume", JSON.stringify(baseData));
-    console.warn("Active resume has been updated successfuly");
-  };
-
-  form.addEventListener("focusout", syncData);
-}
-
-function validateInput(callbacks, target) {
-  const validityState = callbacks.getValidityState(target);
-
-  callbacks.setFieldValidity(target);
-
-  callbacks.displayInputValidity(
-    callbacks,
-    target,
-    validityState.validityMessage
-  );
-}
-
 function addItem(dataset, callbacks, components) {
   const types = {
     degree: "formação",
@@ -85,6 +19,16 @@ function addItem(dataset, callbacks, components) {
     `${types[type]} adicionada com sucesso`,
     "success"
   );
+}
+
+function init({ callbacks, components, models }) {
+  const form = callbacks.getElement("form");
+
+  setInitialState(callbacks, form);
+  setClickActions(callbacks, components, models);
+  setInputMonitoring(callbacks, components, form);
+  setBaseDataSync(callbacks, form);
+  setSubmitMonitoring(callbacks, components, form);
 }
 
 function removeItem(target, callbacks, components) {
@@ -106,4 +50,96 @@ function removeItem(target, callbacks, components) {
     `${types[item]} removida com sucesso`,
     "success"
   );
+}
+
+function setBaseDataSync(callbacks, form) {
+  const syncData = ({ target }) => {
+    const validChanges = ["name", "job", "address", "email", "website", "desc"],
+      shouldNotSync = !validChanges.includes(target.id);
+
+    if (shouldNotSync) return;
+
+    const baseData = callbacks.getBaseData(form);
+    localStorage.setItem("resume", JSON.stringify(baseData));
+    console.warn("Active resume has been updated successfuly");
+  };
+
+  form.addEventListener("focusout", syncData);
+}
+
+function setClickActions(callbacks, components) {
+  const actions = {
+    openMenu: ({ dataset }) => callbacks.openMenu(dataset.dialog),
+    closeMenu: ({ dataset }) => callbacks.closeMenu(dataset.dialog),
+    addItem: ({ dataset }) => addItem(dataset, callbacks, components),
+    displayConfirmation: (target) =>
+      callbacks.displayConfirmation(callbacks, target),
+    removeItem: (target) => removeItem(target, callbacks, components),
+  };
+
+  document.addEventListener("click", ({ target }) => {
+    const { action } = target.dataset;
+
+    if (actions[action]) actions[action](target);
+  });
+}
+
+function setInitialState(callbacks, form) {
+  callbacks.clearActiveResume(form);
+  callbacks.resetIds();
+  callbacks.clearForm(callbacks);
+}
+
+function setInputMonitoring(callbacks, components, form) {
+  form.addEventListener("input", ({ target }) => {
+    callbacks.updateCharacterCount(callbacks, target);
+    validateInput(callbacks, components, target);
+  });
+}
+
+function setSubmitMonitoring(callbacks, components, form) {
+  form.addEventListener("submit", (evt) => {
+    const formIsValid = validateSubmit(callbacks, components, form);
+
+    if (formIsValid) return;
+
+    evt.preventDefault();
+  });
+}
+
+function validateSubmit(callbacks, components, form) {
+  const { elements } = form,
+    fieldsToCheck = ["name", "job", "address", "email", "website", "desc"],
+    invalidFields = [];
+
+  let formIsValid = true;
+
+  for (const field of fieldsToCheck) {
+    const fieldIsValid = validateInput(callbacks, components, elements[field]);
+
+    if (fieldIsValid) continue;
+
+    callbacks.showWarning(
+      components,
+      "Existem campos indevidamente preenchidos",
+      "warning"
+    );
+
+    formIsValid = false;
+
+    invalidFields.push(elements[field]);
+  }
+
+  if (!formIsValid) invalidFields[0].focus();
+
+  return formIsValid;
+}
+
+function validateInput(callbacks, components, target) {
+  const validityState = callbacks.getValidityState(target);
+
+  callbacks.setFieldValidity(validityState);
+  callbacks.setValidityMessage(callbacks, components, validityState);
+
+  return validityState.valid;
 }
